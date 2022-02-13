bfc99c17d4604aa341d53f3724418a3854357e87
Leafs <Leafstorm64@gmail.com>
2022-02-13 17:08:01 -0600

    new commit

---
+++ Assignment3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignment3 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            dateFormatting();
+            Console.WriteLine();
+            piFormatting();
+        }
+        static void dateFormatting() 
+        {
+            DateTime dt = new DateTime(2019, 01, 22, 23, 01, 27);
+            Console.WriteLine($"{dt,40:MMMM,dd,yyyy}");
+            Console.WriteLine($"{dt:yyyy.MM.dd}");
+            Console.WriteLine($"Day {dt:dd} of {dt:MMMM, yyyy}");
+            Console.WriteLine($"Year:{dt:yyyy}, Month:{dt:MM}, Day:{dt:dd}");
+            Console.WriteLine($"{dt,10:dddd}");
+            Console.WriteLine($"{dt,10:hh:mm tt}{dt,10:dddd}");
+            Console.WriteLine($"h:{dt:hh}, m:{dt:mm}, s:{dt:ss}");
+            Console.WriteLine($"{dt:yyyy.MM.dd.hh.mm.ss}");
+        }
+        static void piFormatting() 
+        {
+            var pi = Math.PI;
+            Console.WriteLine($"{pi:c}");
+            Console.WriteLine($"{pi,10:n3}");
+        }
+    }
+}
